9aaba9b5ad54dfec0e19b687c2bdaab7263598f6
Desforges Thomas <69684754+tosbas@users.noreply.github.com>
2023-08-13 12:17:14 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,104 +1,37 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext("2d");
 
-const points = document.getElementById("points");
-let points_numbers = document.getElementById("points_numbers");
+const points_numbers = document.getElementById("points_numbers");
+const messageBox = document.getElementById("messageBox");
+const textInMessageBox = document.getElementById("text");
+const bouton_start = document.getElementById("bouton_start");
 
-const div_text = document.createElement("div");
-div_text.setAttribute("id", "div_text");
-const button_restart = document.createElement("button");
-button_restart.innerText = "Rejouer";
+let start = false;
 
 canvas.width = 600;
 canvas.height = 600;
 
-let start = true;
+const paddleWidth = 100;
+const paddleHeight = 10;
+const paddleSpeed = 7;
+const ballRadius = 10;
+const ballSpeed = 5;
 
-if (start == true) {
-
-    const paddle = {
-        x: canvas.width / 2,
-        y: canvas.height - 20,
-        width: 150,
-        height: 30,
-    }
-
-    function draw_paddle() {
-        ctx.fillStyle = "red";
-        ctx.beginPath();
-        ctx.fillRect(paddle.x - paddle.width / 2, paddle.y, paddle.width, paddle.height)
-        ctx.fill();
-
-    }
-
-    document.addEventListener('keydown', (e) => {
-        if (e.keyCode == 37 && paddle.x - paddle.width / 2 > 0) {
-            paddle.x -= 100;
-        } else if (e.keyCode == 39 && paddle.x != canvas.width) {
-            paddle.x += 100
-        }
-    })
-
-    // objet ball
-
-    const ball = {
-        x: canvas.width / 2,
-        y: canvas.height / 2,
-        radius: 10,
-        speedX: Math.random() * 2 + 1.5,
-        speedY: 2,
-    }
-
-    function drawBall() {
-        ctx.fillStyle = "red";
-        ctx.beginPath();
-        ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
-        ctx.fill();
-
-        if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) {
-            ball.speedX = -ball.speedX;
-        } else if (ball.y - ball.radius < 0 || ball.y + ball.radius >= paddle.y && ball.x >= paddle.x - paddle.width / 2 && ball.x <= paddle.x + paddle.width / 2) {
-            ball.speedY = -ball.speedY;
-        } else if (ball.y > canvas.height) {
-
-            start = false;
-
-        } else {
-            for (let i = 0; i < array.length; i++) {
-                if (ball.x <= array[i].x + array[i].width && // ball.x <= brique.x + brique.width
-                    ball.x >= array[i].x && // ball.y + ball.width >= brique.x
-                    ball.y <= array[i].y + array[i].height &&
-                    ball.y >= array[i].y) { // ball.x + ball.height >= brique.y
-                    ball.speedY = -ball.speedY;
-                    array.splice(i, 1)
-                    points_numbers.innerText++;
-
-                }
-            }
-
-        }
-
-        ball.x += ball.speedX;
-        ball.y += ball.speedY;
-
-    }
-
-}
-
-// class brique 
+let rightPressed = false;
+let leftPressed = false;
 
 class Brique {
-    constructor(x, y) {
+    constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
-        this.width = 100;
-        this.height = 30;
-        this.speedX = 1;
+        this.width = width;
+        this.height = height;
     }
+
     draw() {
         ctx.fillStyle = "red";
         ctx.strokeStyle = "white";
-        ctx.strokeRect(this.x, this.y, this.width, this.height)
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
         ctx.beginPath();
         ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.fill();
@@ -106,71 +39,194 @@ class Brique {
     }
 }
 
-// tableau contenant chaque briques
-
-const array = [];
-
-let x = 0
-let y = 20
-
-for (let i = 0; i < 30; i++) {
-    array.push(new Brique(x, y));
-    x += 100
-
-    if (array[i].x + array[i].width >= canvas.width) {
-        x = 0;
-        y += 50;
+class Paddle {
+    constructor() {
+        this.x = canvas.width / 2 - paddleWidth / 2;
+        this.y = canvas.height - paddleHeight;
+        this.width = paddleWidth;
+        this.height = paddleHeight;
+        this.speed = paddleSpeed;
     }
 
-}
-
-// function qui anime le tout 
-
-function anim() {
-
-    if (start == true) {
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        requestAnimationFrame(anim);
-
-        for (let i = 0; i < array.length; i++) {
-            array[i].draw();
-        }
-
-        drawBall();
-        draw_paddle();
-
-
-        if (array.length == 0) {
-            setTimeout(() => {
-
-                start = false;
-
-            }, 100)
-        }
-
-
-    } else if (start == false) {
-
-        document.querySelector("body").appendChild(div_text);
-
-        if (array.length == 0) {
-
-            div_text.innerText = "Congratulation !";
-            div_text.appendChild(button_restart);
-
-        } else if (array.length != 0) {
-            div_text.innerText = "Vous avez perdu ! ";
-            div_text.appendChild(button_restart);
-        }
+    draw() {
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fill();
     }
 
+    move() {
+        if (rightPressed && this.x < canvas.width - this.width) {
+            this.x += this.speed;
+        }
+        else if (leftPressed && this.x > 0) {
+            this.x -= this.speed;
+        }
+    }
+};
+
+const paddle = new Paddle();
+
+class Ball {
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speedX = ballSpeed;
+        this.speedY = ballSpeed;
+    }
+
+    draw() {
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    move() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.speedX = -this.speedX;
+        }
+
+        if (this.y - this.radius < 0) {
+            this.speedY = -this.speedY;
+        }
+
+        if (this.y + this.radius > paddle.y &&
+            this.y - this.radius < paddle.y + paddle.height &&
+            this.x + this.radius > paddle.x &&
+            this.x - this.radius < paddle.x + paddle.width
+        ) {
+
+            this.speedY = -this.speedY;
+        }
+
+        if (this.speedX > ballSpeed) {
+            this.speedX = ballSpeed;
+        }
+
+        this.draw();
+
+        if (this.y + this.radius > canvas.height) {
+            this.y = canvas.height - this.radius;
+            gameOver();
+            return true;
+        }
+
+        return false;
+    }
 }
 
-anim();
+let ball = new Ball(canvas.width / 2, canvas.height - paddleHeight - ballRadius, ballRadius);
+let bricks = [];
 
-// relancer le jeu
+function createBricks() {
+    const posXStartBrique = 10;
+    const posYStartBrique = 10;
+    const height = 20;
+    const maxRow = 5;
+    const maxBrique = 5;
+    const spaceY = 10;
+    const spaceX = 10;
 
-button_restart.addEventListener("click", () => {
-    location.reload();
-})
+    let posX = posXStartBrique;
+    let posY = posYStartBrique;
+
+    for (let i = 0; i < maxRow; i++) {
+        for (let j = 0; j < maxBrique; j++) {
+            const width = (canvas.width - (maxBrique - 1) * spaceX - 2 * posXStartBrique) / maxBrique;
+
+            bricks.push(new Brique(posX, posY, width, height));
+
+            if (j < maxBrique - 1) {
+                posX += width + spaceX;
+            }
+        }
+
+        posX = posXStartBrique;
+        posY += height + spaceY;
+    }
+}
+
+function draw() {
+    ctx.fillStyle = "rgba(0,0,0,0.5)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ball.draw();
+    paddle.draw();
+    bricks.forEach(brick => brick.draw());
+
+    if (start) {
+        const gameOver = ball.move();
+
+        if (!gameOver) {
+            for (let i = 0; i < bricks.length; i++) {
+                if (
+                    ball.x + ball.radius > bricks[i].x &&
+                    ball.x - ball.radius < bricks[i].x + bricks[i].width &&
+                    ball.y + ball.radius > bricks[i].y &&
+                    ball.y - ball.radius < bricks[i].y + bricks[i].height
+                ) {
+                    bricks.splice(i, 1);
+                    points_numbers.innerText++;
+                    ball.speedY = -ball.speedY;
+                    break;
+                }
+            }
+        }
+
+        paddle.move();
+    }
+
+    requestAnimationFrame(draw);
+}
+
+const gameOver = () => {
+    start = false;
+    textInMessageBox.textContent = "Vous avez perdu !";
+    messageBox.classList.remove("cacheText");
+}
+
+const showMessage = (message) => {
+    textInMessageBox.textContent = message;
+    messageBox.classList.toggle("cacheText");
+}
+
+
+
+const keyDownHandler = (e) => {
+    if (e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+const keyUpHandler = (e) => {
+    if (e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+
+bouton_start.addEventListener("click", () => {
+    ball = new Ball(canvas.width / 2, canvas.height - paddleHeight - ballRadius, ballRadius);
+    paddle.x = canvas.width / 2 - paddleWidth / 2;
+    bricks = [];
+    createBricks();
+    points_numbers.textContent = 0;
+    showMessage("");
+    start = true;
+});
+
+createBricks();
+draw();
